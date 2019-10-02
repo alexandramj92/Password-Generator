548039d9f9e469c120af58180a973a4fc084b0a0
Alexandra Luna <alexandra.michelle.j@gmail.com>
2019-10-02 08:25:45 -0700

    added code to generate random password based on character type selection to javascript file

--- README.md
+++ README.md
@@ -11,6 +11,60 @@
 
 ## Usage
 
+The display shows an empty textarea with two buttons underneath. One button is labeled "Generate Password" and the other is labeled "Copy to Clipboard". 
+
+When the user clicks on the "Generate Password" button, a prompt appears asking the user the length of the password. They must enter a number between 8 and 128. A second prompt appears asking the user if they would like to use Special Characters, then if they would like to use numeric characters, then if they would like to use lowercase characters, and finally if they would like to use uppercase characters. 
+
+The user will be required to use at least one character type. 
+
+Once this criteria is recorded. The password is generated and displayed in the text area. 
+
+The user can then click on the "Copy to Clipboard" button to copy the password that appeared in the text area. 
+
+The html is linked to bootstrap and a custom css stylesheet in order to ensure the application is responsive at different sizes. 
+
+
+Elements needed:
+
+A variable that stores the user's desired password length. It can only accept a number between 8 and 128.
+ 
+An array of all the special characters.
+
+An array of all the numeric characters.
+
+An array of all lowecase characters. 
+
+An array of all Uppercase characters.
+
+A variable that stores the user's desired character type(s). 
+
+A for loop that will randomly select a combination of Character types depending on the user's selection. 
+
+Once the random combination of characters is generated according to the user's selected length, it will display in the html text area
+
+
+Different scenarios:
+
+User chooses all four character types. 
+
+User just chooses special characters.
+
+User just chooses numeric characters.
+
+User just chooses lowercase characters. 
+
+user just chooses uppercase characters. 
+
+
+User chooses any combination of the above. 
+
+
+
+
+
+
+
+
 
 
 
---
+++ passwordgenerator.js
@@ -0,0 +1,145 @@
+
+//Special Characters User Input
+
+
+function checkSpecialChar(){
+
+    console.log("Special function is called");
+if (document.getElementById("inlineCheckbox1").checked){
+
+   specialChar = "!@#$%^&*!@#$%^&*!@#$%^&*&";
+    console.log("Special characters is true");
+}
+
+else {
+    specialChar = "";
+}
+
+}
+
+if (document.getElementById("inlineCheckbox1").checked == false){
+    console.log("special characters not checked")
+    specialChar = "";
+}
+
+
+
+//Numeric Characters User Input
+
+function checkNumericChar(){
+    console.log(" Numeric function is called");
+
+
+if (document.getElementById("inlineCheckbox2").checked){
+
+    numericChar = "0123456789012345678956789";
+    console.log("Numeric characters is true");
+
+}
+
+else {
+    numericChar = "";
+}
+
+}
+
+//Numeric Characters if box unchecked
+
+if (document.getElementById("inlineCheckbox2").checked == false) {
+    console.log("numeric characters not checked");
+    numericChar = "";
+}
+
+
+//Lowercase Characters User Input
+function checkLowerChar(){
+    console.log("Lowercase function is called");
+
+if (document.getElementById("inlineCheckbox3").checked){
+
+    lowercaseChar = "abcdefghijklmnopqrstuvxyz";
+    console.log("Lowercase characters is true");
+}
+
+else {
+    lowercaseChar = "";
+}
+
+}
+
+//Lowercase Characters if box unchecked
+
+if (document.getElementById("inlineCheckbox3").checked == false) {
+    console.log("lowercase characters not checked");
+    lowercaseChar = "";
+}
+
+
+//Uppercase Characters User Input
+function checkUpperChar(){
+    console.log("Uppercase funtion is called")
+
+if (document.getElementById("inlineCheckbox4").checked){
+
+    uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    console.log("Uppercase characters is true");
+}
+
+else {
+    uppercaseChar = "";
+}
+
+}
+
+//Uppercase Characters if box unchecked
+
+if (document.getElementById("inlineCheckbox4").checked == false) {
+    console.log("uppercase characters not checked");
+    uppercaseChar = "";
+}
+
+
+function generatePassword(){
+        console.log("Generate Password function is called");
+
+        let passlength = document.getElementById("slider").value;
+
+        //Value for generatePassword function 
+
+        let value = specialChar + numericChar + lowercaseChar + uppercaseChar;
+        let password= ""; 
+
+        for (var i = 0; i < passlength; i++){
+            password = password + value.charAt(Math.floor(Math.random() * Math.floor(value.length)));
+        }
+
+        document.getElementById("display").value = password;
+
+}
+
+
+//set default length display of 25
+document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+
+//function to set length based on slider position
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+
+}
+
+//function to copy password to clipboard
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+
+}
